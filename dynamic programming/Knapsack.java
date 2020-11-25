778981348b865b9dd896960714e4150626081f87
arleserp <arles.rodriguez@gmail.com>
2020-11-25 11:35:43 -0500

    Update Knapsack.java

--- dynamic programming/Knapsack.java
+++ dynamic programming/Knapsack.java
@@ -5,6 +5,8 @@
  */
 package dp;
 
+import java.util.Arrays;
+
 /**
  *
  * @author Arles
@@ -14,25 +16,34 @@ public class Knapsack {
     static int N; //Numero de objetos 
     static int prices[]; //precios de cada producto
     static int weights[]; //pesos de cada producto
-    static int memo[][]; //DEBE INICIALIZARSE EN -1 y/o null
+    static long memo[][]; //DEBE INICIALIZARSE EN -1 y/o null
 
-
-    static int knapsack(int id, int w) {
+    static long knapsack(int id, int w) {
         if (id == N || w == 0) {
-            return 0;    
+            return 0;
+        }
+        if (memo[id][w] != -1) {
+            return memo[id][w];
         }
         if (weights[id] > w) {
-            return knapsack(id + 1, w);
+            return memo[id][w] = knapsack(id + 1, w);
         } else {
-            return  Math.max(knapsack(id + 1, w), prices[id] + knapsack(id + 1, w - weights[id]));
+            return memo[id][w] = Math.max(knapsack(id + 1, w), prices[id] + knapsack(id + 1, w - weights[id]));
         }
     }
-    
+
+    static void init() {
+        for (int i = 0; i < N; i++) {
+            Arrays.fill(memo[i], -1);
+        }
+    }
+
     public static void main(String[] args) {
         int W = 15;
         N = 5;
         weights = new int[N];
         prices = new int[N];
+        memo = new long[N+1][W+1];
         weights[0] = 12;
         weights[1] = 2;
         weights[2] = 1;
@@ -42,9 +53,9 @@ public class Knapsack {
         prices[1] = 2;
         prices[2] = 2;
         prices[3] = 1;
-        prices[4] =10;
-        System.out.println(knapsack(0,W));
-        
-        
+        prices[4] = 10;
+        init();
+        System.out.println(knapsack(0, W));
+
     }
 }
